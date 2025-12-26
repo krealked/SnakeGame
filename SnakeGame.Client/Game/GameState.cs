ed0a639e30db04d535f2bf965d801840e4a5bfdc
Егор Баскаков < @DESKTOP-QIQR749>
2025-12-26 22:13:03 +0300

    Add advanced graphics: smooth turns, eat animation, eyes,

--- SnakeGame.Client/Game/GameState.cs
+++ SnakeGame.Client/Game/GameState.cs
@@ -4,8 +4,8 @@ public class GameState
 {
     public int Width { get; set; }
     public int Height { get; set; }
-    public Snake Snake { get; set; }
-    public Point Food { get; set; }
+    public required Snake Snake { get; set; }
+    public required Point Food { get; set; }
     public bool IsGameOver { get; set; }
     public int Score { get; set; }
 }
